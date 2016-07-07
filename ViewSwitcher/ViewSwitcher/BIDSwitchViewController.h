39480c60456e138d2793e452beaa7437bfcf3ec8
Nigel Lee <lihui8@le.com>
2016-07-07 11:54:28 +0800

    add VC into that empty application

---
+++ ViewSwitcher/ViewSwitcher/BIDSwitchViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDSwitchViewController.h
+//  ViewSwitcher
+//
+//  Created by Nigel Lee on 16/7/7.
+//  Copyright © 2016年 Apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDSwitchViewController : UIViewController
+
+@end
---
+++ ViewSwitcher/ViewSwitcher/BIDSwitchViewController.m
@@ -0,0 +1,37 @@
+//
+//  BIDSwitchViewController.m
+//  ViewSwitcher
+//
+//  Created by Nigel Lee on 16/7/7.
+//  Copyright © 2016年 Apress. All rights reserved.
+//
+
+#import "BIDSwitchViewController.h"
+
+@interface BIDSwitchViewController ()
+
+@end
+
+@implementation BIDSwitchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
